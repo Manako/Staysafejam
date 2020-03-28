d2429031712af90b231ab421aa6cba09e846210d
Miguel Mendes <miguelgamendes@gmail.com>
2020-03-28 16:29:08 +0000

    Added timer to level. Does nothing at 0.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float gameTime;
+    [SerializeField] private Text timerText;
+
+    private void Update()
+    {
+        this.gameTime -= Time.deltaTime;
+        timerText.text = ((int)this.gameTime).ToString();
+    }
+}
